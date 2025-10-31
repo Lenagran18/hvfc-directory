216b838b02ebc30b7eb6a94c765208494c210c4d
Lenagran18 <Lenagran18@gmail.com>
2025-10-30 20:31:08 -0400

    Setup MemberCrewDirectory component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import MemberCrewDirectory from "./components/MemberCrewDirectory";
 
 function App() {
-
+    return <MemberCrewDirectory />;
 }
 
 export default App;
---
+++ src/components/MemberCrewDirectory.jsx
@@ -0,0 +1,4 @@
+const MemberCrewDirectory = () => {
+
+};
+export default MemberCrewDirectory;
