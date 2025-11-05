05d33fe246ea085076ae8f4b82b11b73da205e95
Lenagran18 <Lenagran18@gmail.com>
2025-11-04 21:37:18 -0500

    Add root path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import MemberCrewDirectory from "./components/MemberCrewDirectory";
 import NonMemberCrewDirectory from "./components/NonMemberCrewDirectory";
 import LocationDirectory from "./components/LocationDirectory";
 import JobBoard from "./components/JobBoard";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
     return ( 
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to="/MemberCrewDirectory" replace />} />
                 <Route path="/MemberCrewDirectory" element={<MemberCrewDirectory />} />
                 <Route path="/NonMemberCrewDirectory" element={<NonMemberCrewDirectory />} />
                 <Route path="/LocationDirectory" element={<LocationDirectory />} />
